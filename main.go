ecbc7bb63d8d0610f3e3fafb0780c9665043476c
Rohinish <singhrohinish09@gmail.com>
2023-02-22 00:53:24 +0530

    learned about useinput,arrays nd slice

--- main.go
+++ main.go
@@ -2,14 +2,39 @@ package main
 import "fmt"
 
 func main(){
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("Welcome to %v booking application\n",conferenceName)
 
 	fmt.Printf("We have total of %v tickets and %v are still available\n",conferenceTickets,remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var bookings []string
+	
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	fmt.Println("Enter your first name-")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name-")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email-")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets you want to buy-")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	// bookings = firstName+" "+lastName
+	bookings = append(bookings,firstName+" "+lastName)  //slice
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v.\n",firstName,lastName,userTickets,email)
+
+	fmt.Printf("%v tickets remaining for %v.\n",remainingTickets,conferenceName)
+	
 
 
 }
